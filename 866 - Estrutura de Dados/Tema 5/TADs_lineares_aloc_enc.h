61c3fcc5bc37f7478a85082403371519c088d1a3
Mateus Santos <m.santos.azeve@gmail.com>
2021-03-29 23:11:39 -0300

    ambiente preparado para fila

--- 866 - Estrutura de Dados/Tema 5/TADs_lineares_aloc_enc.h
+++ 866 - Estrutura de Dados/Tema 5/TADs_lineares_aloc_enc.h
@@ -1,7 +1,7 @@
-//TADs lineares (lista, pilha e fila) com alocação encadeada (dinâmica)
+//TADs lineares (lista, pilha e fila) com alocaï¿½ï¿½o encadeada (dinï¿½mica)
 
 //tipo exportado
-//define o TipoItem que é o tipo dos elementos dos TADs
+//define o TipoItem que ï¿½ o tipo dos elementos dos TADs
 typedef struct
 {
     int chave;
@@ -9,80 +9,28 @@ typedef struct
 }TipoItem;
 
 //tipo exportado
-//define o TipoCelula que é o tipo das células que contêm os elementos que são manipulados pelos TADs
+//define o TipoCelula que ï¿½ o tipo das cï¿½lulas que contï¿½m os elementos que sï¿½o manipulados pelos TADs
 typedef struct celula
 {
     TipoItem item;
     struct celula *proximo;
 }TipoCelula;
 
-//-----TAD lista-----
-
-//tipo exportado
-//define o TipoLista que é o tipo do TAD lista
-typedef struct
-{
-    TipoCelula *primeiro;
-    TipoCelula *ultimo;
-}TipoLista;
-
-//funções exportadas
-//cria uma lista vazia
-void CriaListaVazia(TipoLista *lista);
-
-//testa se a lista é vazia
-int TestaListaVazia(TipoLista *lista);
-
-//insere um elemento do tipo TipoItem na lista
-void InsereLista(TipoLista *lista, TipoItem item);
-
-//remove um elemento do tipo TipoItem da lista
-void RemoveLista(TipoLista *lista, TipoItem *item, TipoCelula *pt);
-
-//imprime os elementos da lista
-void ImprimeLista(TipoLista *lista);
-
-//-----TAD pilha-----
-
-//tipo exportado
-//define o TipoPilha que é o tipo do TAD pilha
-typedef struct
-{
-    TipoCelula *topo;
-    TipoCelula *fundo;
-}TipoPilha;
-
-//funções exportadas
-//cria uma pilha vazia
-void CriaPilhaVazia(TipoPilha *pilha);
-
-//testa se a pilha é vazia
-int TestaPilhaVazia(TipoPilha *pilha);
-
-//insere um elemento do tipo TipoItem na pilha
-void InserePilha(TipoPilha *pilha, TipoItem item);
-
-//remove um elemento do tipo TipoItem da pilha
-void RemovePilha(TipoPilha *pilha, TipoItem *item);
-
-//imprime os elementos da pilha
-void ImprimePilha(TipoPilha *pilha);
-
 //-----TAD fila-----
 
 //tipo exportado
-//define o TipoFila que é o tipo do TAD fila
+//define o TipoFila que ï¿½ o tipo do TAD fila
 typedef struct
 {
     TipoCelula *frente;
     TipoCelula *tras;
 }TipoFila;
 
-//funções exportadas
+//funï¿½ï¿½es exportadas
 //cria uma fila vazia
 void CriaFilaVazia(TipoFila *fila);
 
-//testa se a fila é vazia
+//testa se a fila ï¿½ vazia
 int TestaFilaVazia(TipoFila *fila);
 
 //insere um elemento do tipo TipoItem na fila
