ed8d3eb2aeaaa983675cb79393441085e4e51096
Mateus Santos <m.santos.azeve@gmail.com>
2021-04-16 20:22:16 -0300

    faltando a impressao dos resultados

--- 866 - Estrutura de Dados/AV1/Exercicio1/main.c
+++ 866 - Estrutura de Dados/AV1/Exercicio1/main.c
@@ -3,15 +3,53 @@
 #include "TAD_lista_aloc_enc.h"
 
 
-// TipoLista * opera(TipoLista *p1, TipoLista *p2, int op);
+TipoLista * opera(TipoLista *p1, TipoLista *p2, int op)
+{
+    TipoLista * resultado = (TipoLista *)malloc(sizeof(TipoLista));
+    CriaListaVazia(resultado);
+
+    TipoCelula *aux = p1->primeiro->proximo;
+    TipoCelula *aux2 = p2->primeiro->proximo;
+    TipoItem valor_operacao;
+
+    while (aux != NULL)
+    {
+        while (aux2 != NULL)
+        {
+            // SOMA
+            if (op == 0)
+            {
+                if(aux->item.grau == aux2->item.grau)
+                {
+                    valor_operacao.coeficiente = aux->item.coeficiente + aux2->item.coeficiente;
+                    valor_operacao.grau = aux->item.grau;
+                }
+            } 
+            // SUBTRACAO
+            else if (op == 1) 
+            {
+                if(aux->item.grau == aux2->item.grau)
+                {
+                    valor_operacao.coeficiente = aux->item.coeficiente - aux2->item.coeficiente;
+                    valor_operacao.grau = aux->item.grau;
+                }
+            }
+        }
+    }
+
+    return resultado;
+}
 
 int main(void)
 {
     TipoLista *polinomio1 = (TipoLista *)malloc(sizeof(TipoLista));
     TipoLista *polinomio2 = (TipoLista *)malloc(sizeof(TipoLista));
-    TipoLista *resultado = (TipoLista *)malloc(sizeof(TipoLista));
+    TipoLista *resultadoSoma = (TipoLista *)malloc(sizeof(TipoLista));
+    TipoLista *resultadoSub = (TipoLista *)malloc(sizeof(TipoLista));
     CriaListaVazia(polinomio1);
     CriaListaVazia(polinomio2);
+    CriaListaVazia(resultadoSoma);
+    CriaListaVazia(resultadoSub);
     TipoItem item;
     TipoItem item2;
 
@@ -50,39 +88,19 @@ int main(void)
         InsereLista(polinomio2, item2);
     } while (1);
     
-    printf("Polinomio1: ");
-    ImprimeLista(polinomio1);
-    printf("\nPolinomio2: ");
-    ImprimeLista(polinomio2);   
+    printf("\n\nDeveria ter o resultado abaixo\n\n");
+    // printf("Polinomio1: ");
+    // ImprimeLista(polinomio1);
+    // printf("\nPolinomio2: ");
+    // ImprimeLista(polinomio2);   
 
-        //SOMA
-        // resultado = opera(polinomio1, polinomio2, 0);
-        // ImprimeLista(resultado);
+    // SOMA
+    resultadoSoma = opera(polinomio1, polinomio2, 0);
+    ImprimeLista(resultadoSoma);
 
-        // //SUBTRAÇÃO
-        // resultado = opera(polinomio1, polinomio2, 1);
-        // ImprimeLista(resultado);
+    // SUBTRAÇÃO
+    resultadoSub = opera(polinomio1, polinomio2, 1);
+    ImprimeLista(resultadoSub);
 
     return 0;
 }
-
-// TipoLista *opera(TipoLista *p1, TipoLista *p2, int op);
-// {
-//     TipoLista *resultado = (TipoLista *)malloc(sizeoff(TipoLista));
-//     CriaListaVazia(resultado);
-    
-//      switch(op)
-//      {
-//         case '0':
-//             printf("%d" ,polinomio1 + polinomio2);
-//             break;
-
-//         case '1':
-//             printf("%d" ,polinomio1 - polinomio2);
-//             break;
-//      }
-        
-
-
-//     return resultado;
-// }
