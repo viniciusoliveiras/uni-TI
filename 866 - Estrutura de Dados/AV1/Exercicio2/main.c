e4ea98a346f580cfa813ccb2f2ea4e8ff1f342a6
Mateus Santos <m.santos.azeve@gmail.com>
2021-04-16 14:58:15 -0300

    pilha funcionando

--- 866 - Estrutura de Dados/AV1/Exercicio2/main.c
+++ 866 - Estrutura de Dados/AV1/Exercicio2/main.c
@@ -23,6 +23,27 @@ TipoFila * le_expressao(char *exp)
     return expressao;
 }
 
+int avalia_expressao(TipoFila *exp)
+{
+    TipoPilha * pilha = (TipoPilha *)malloc(sizeof(TipoPilha));
+    TipoCelula *aux = exp->frente->proximo;
+    CriaPilhaVazia(pilha);
+    int valor_digitado;
+
+    while (aux != NULL)
+    {
+        if (aux->item.valor >= 'A' || aux->item.valor <= 'E')
+        {
+            printf("Digite o valor de %c: ", aux->item.valor);
+            scanf("%d", &valor_digitado);
+            aux->item.valor = valor_digitado;
+            InserePilha(pilha, aux->item);
+            aux = aux->proximo;
+        }
+    }
+    ImprimePilha(pilha);
+}
+
 int main()
 {
     char elemento[30];
@@ -36,6 +57,7 @@ int main()
     
     TipoFila * fila = le_expressao(elemento);
     ImprimeFila(fila);
+    avalia_expressao(fila);  
 
     return 0;
 }
