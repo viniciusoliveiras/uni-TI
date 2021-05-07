fa3799d886557ddcaaea7a1023094218bf29eb78
Mateus Santos <m.santos.azeve@gmail.com>
2021-05-06 21:31:24 -0300

    avanco atividade do tema 7

--- 866 - Estrutura de Dados/Tema 7/main.c
+++ 866 - Estrutura de Dados/Tema 7/main.c
@@ -8,7 +8,7 @@ int main(void)
     setlocale(LC_ALL, "");
 
     printf("------------------\n");
-    printf("  Árvore Binária\n");
+    printf("  Arvore Binaria\n");
     printf("------------------\n");
 
     TipoArvore **arvore_01 = (TipoArvore **)malloc(sizeof(TipoArvore *));
@@ -24,97 +24,85 @@ int main(void)
     TipoArvore **arvore_11 = (TipoArvore **)malloc(sizeof(TipoArvore *));
     TipoArvore **arvore_12 = (TipoArvore **)malloc(sizeof(TipoArvore *));
     TipoArvore **arvore_13 = (TipoArvore **)malloc(sizeof(TipoArvore *));
+    TipoArvore **arvore_14 = (TipoArvore **)malloc(sizeof(TipoArvore *));
+    TipoArvore **arvore_15 = (TipoArvore **)malloc(sizeof(TipoArvore *));
+    TipoArvore **arvore_16 = (TipoArvore **)malloc(sizeof(TipoArvore *));
+    TipoArvore **arvore_17 = (TipoArvore **)malloc(sizeof(TipoArvore *));
+    TipoArvore **arvore_18 = (TipoArvore **)malloc(sizeof(TipoArvore *));
+    TipoArvore **arvore_19 = (TipoArvore **)malloc(sizeof(TipoArvore *));
 
+    //sub-arvore D
     CriaArvoreBinariaVazia(arvore_01);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_01));
-
     CriaArvoreBinariaVazia(arvore_02);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_02));
-
-    //sub-árvore D
     CriaArvoreBinariaVazia(arvore_03);
     TipoItem e1;
     e1.chave = 'd';
     CriaArvoreBinaria(e1, arvore_03, arvore_01, arvore_02);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_03));
 
+    //sub-arvore C
     CriaArvoreBinariaVazia(arvore_04);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_04));
-
     CriaArvoreBinariaVazia(arvore_05);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_05));
-
-    //sub-árvore E
     CriaArvoreBinariaVazia(arvore_06);
     TipoItem e2;
-    e2.chave = 'e';
+    e2.chave = 'c';
     CriaArvoreBinaria(e2, arvore_06, arvore_04, arvore_05);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_06));
 
+    //sub-arvore /
     CriaArvoreBinariaVazia(arvore_07);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_07));
-
-    CriaArvoreBinariaVazia(arvore_08);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_08));
-
-    //sub-árvore F
-    CriaArvoreBinariaVazia(arvore_09);
     TipoItem e3;
-    e3.chave = 'f';
-    CriaArvoreBinaria(e3, arvore_09, arvore_07, arvore_08);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_09));
+    e3.chave = '/';
+    CriaArvoreBinaria(e3, arvore_07, arvore_03, arvore_06);
 
+    //sub-arvore E
+    CriaArvoreBinariaVazia(arvore_08);
+    CriaArvoreBinariaVazia(arvore_09);
     CriaArvoreBinariaVazia(arvore_10);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_10));
-
-    //sub-árvore B
-    CriaArvoreBinariaVazia(arvore_11);
     TipoItem e4;
-    e4.chave = 'b';
-    CriaArvoreBinaria(e4, arvore_11, arvore_10, arvore_03);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_11));
+    e4.chave = 'e';
+    CriaArvoreBinaria(e4, arvore_10, arvore_08, arvore_09);
 
-    //sub-árvore C
-    CriaArvoreBinariaVazia(arvore_12);
+    //sub-arvore -
+    CriaArvoreBinariaVazia(arvore_11);
     TipoItem e5;
-    e5.chave = 'c';
-    CriaArvoreBinaria(e5, arvore_12, arvore_06, arvore_09);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_12));
+    e5.chave = '-';
+    CriaArvoreBinaria(e5, arvore_11, arvore_10, arvore_07);
 
-    //árvore A
+    //sub-arvore B
+    CriaArvoreBinariaVazia(arvore_12);
     CriaArvoreBinariaVazia(arvore_13);
+    CriaArvoreBinariaVazia(arvore_14);
     TipoItem e6;
-    e6.chave = 'a';
-    CriaArvoreBinaria(e6, arvore_13, arvore_11, arvore_12);
-    printf("\n%i", TestaArvoreBinariaVazia(arvore_13));
+    e6.chave = 'b';
+    CriaArvoreBinaria(e6, arvore_14, arvore_12, arvore_13);
 
-    int s = PertenceArvoreBinaria(arvore_13, e3);
-    printf("\n%i", s);
+    //sub-arvore *
+    CriaArvoreBinariaVazia(arvore_15);
+    TipoItem e7;
+    e7.chave = '*';
+    CriaArvoreBinaria(e7, arvore_15, arvore_11, arvore_14);
 
-    printf("\n");
-    ImprimeArvoreBinaria(arvore_13, 0);
+    //sub-arvore A
+    CriaArvoreBinariaVazia(arvore_16);
+    CriaArvoreBinariaVazia(arvore_17);
+    CriaArvoreBinariaVazia(arvore_18);
+    TipoItem e8;
+    e8.chave = 'a';
+    CriaArvoreBinaria(e8, arvore_18, arvore_16, arvore_17);
 
-    printf("\n");
-    PercorreArvoreBinariaPreOrdem(arvore_13);
-    printf("\n");
-    PercorreArvoreBinariaOrdemSimetrica(arvore_13);
-    printf("\n");
-    PercorreArvoreBinariaPosOrdem(arvore_13);
-    printf("\n");
-
-    PercorreArvoreBinariaLargura(arvore_13);
-    printf("\n");
-
-    PercorreArvoreBinariaProfundidade(arvore_13);
-    printf("\n");
-
-    *arvore_12 = LiberaArvoreBinaria(arvore_12);
+    //arvore +
+    CriaArvoreBinariaVazia(arvore_19);
+    TipoItem e9;
+    e9.chave = '+';
+    CriaArvoreBinaria(e9, arvore_19, arvore_15, arvore_18);
 
     printf("\n");
-    ImprimeArvoreBinaria(arvore_13, 0);
+    ImprimeArvoreBinaria(arvore_19, 0);
 
-    int h = CalculaAlturaArvoreBinaria(arvore_13);
-    printf("\n%i", h);
+    printf("\n");
+    PercorreArvoreBinariaPreOrdem(arvore_19);
+    printf("\n");
+    PercorreArvoreBinariaPosOrdem(arvore_19);
+    printf("\n");
 
     return 0;
 }
