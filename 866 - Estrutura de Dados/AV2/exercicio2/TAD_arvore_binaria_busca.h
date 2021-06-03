78cc47a5c4de6ec11df56bf72f2c994c176a765a
Mateus Santos <m.santos.azeve@gmail.com>
2021-06-03 16:04:31 -0300

    EDAV2EX2 - Organiza os comentarios

--- 866 - Estrutura de Dados/AV2/exercicio2/TAD_arvore_binaria_busca.h
+++ 866 - Estrutura de Dados/AV2/exercicio2/TAD_arvore_binaria_busca.h
@@ -1,7 +1,7 @@
-//TAD árvore binária de busca com alocação encadeada (dinâmica)
+//TAD arvore binaria de busca com alocacao encadeada (dinamica)
 
 //tipo exportado
-//define o TipoItem que é o tipo dos elementos do TAD árvore binária de busca (clássica, AVL e B)
+//define o TipoItem que eh o tipo dos elementos do TAD arvore binaria de busca (classica, AVL e B)
 typedef struct
 {
     int chave;
@@ -10,32 +10,32 @@ typedef struct
 }TipoItem;
 
 //tipo exportado
-//define o TipoCelula que é o tipo das células que contêm os elementos que são manipulados pelo TAD árvore binária de busca (clássica ou AVL)
+//define o TipoCelula que eh o tipo das celulas que contem os elementos que sao manipulados pelo TAD arvore binaria de busca (classica ou AVL)
 typedef struct celula
 {
     TipoItem item;
     struct celula *esquerda;
     struct celula *direita;
-    int balanco; // para o caso de a árvore ser AVL
+    int balanco; // para o caso de a arvore ser AVL
 }TipoCelula;
 
-//-----TAD árvore binária de busca-----
+//-----TAD arvore binaria de busca-----
 
-//funções exportadas
-//cria uma árvore binária de busca vazia
+//funcoes exportadas
+//cria uma arvore binaria de busca vazia
 void CriaArvoreBinariaBuscaVazia(TipoCelula **arvore);
 
-//testa se a árvore binária de busca é vazia
+//testa se a arvore binaria de busca eh vazia
 int TestaArvoreBinariaBuscaVazia(TipoCelula **arvore);
 
-//busca por um elemento na árvore binária de busca
+//busca por um elemento na arvore binaria de busca
 TipoItem * BuscaArvoreBinariaBusca(TipoCelula **arvore, int chave);
 
-//insere um elemento do tipo TipoItem na árvore binária de busca
+//insere um elemento do tipo TipoItem na arvore binaria de busca
 void InsereArvoreBinariaBusca(TipoCelula **arvore, TipoItem item);
 
-//remove um elemento do tipo TipoItem da árvore binária de busca
+//remove um elemento do tipo TipoItem da arvore binaria de busca
 void RemoveArvoreBinariaBusca(TipoCelula **arvore, TipoItem item);
 
-//imprime os elementos da árvore binária de busca
+//imprime os elementos da arvore binaria de busca
 void ImprimeArvoreBinariaBusca(TipoCelula **arvore, int l);
